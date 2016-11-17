fa3d72802902a2b562df6b52336ffc0f44db0909
i3wgnit <i3wgnit@gmail.com>
2016-11-17 00:03:23 -0500

    write.js
    finished test module

--- write.js
+++ write.js
@@ -1,48 +1,73 @@
 function add( num ) {
     var str = "";
-    for ( i = 0; i < Math.abs( num ); i++ ) {
-        str += ( Math.sign( num ) ).toString( 10 ).charAt( 0 );
+    for ( var i = 0; i < Math.abs( num ); i++ ) {
+        str += ( Math.sign( num ) ).toString( 10 ).charAt( 0 ).replace( /1/, "+" );
     }
     return str;
 }
 
 function write( str ) {
-    var poss = [],
-        unq = [],
-        buff = [],
-        bf = "";
 
-    // Setting up input as variable for later parsing
+    // Setting up input as variable for later
     var input = str,
         rst = "";
 
     // Setting up rst as unique chars
     input.split( "" )
-        .forEach( function( el ) {
-        if ( rst.search( el ) < 0 ) {
-            rst += el;
-        }
-    } );
-    rst = rst.split( "" ).sort().join( "" );
+            .forEach( function( el ) {
+            if ( rst.search( el ) < 0 ) {
+                rst += el;
+            }
+        } );
+    rst = rst.split( "" )
+        .join( "" );
 
     // Setting up targets from rst charcode
     var targets = rst.split( "" )
         .map( function( el ) {
-        return el.charCodeAt( 0 );
-    } );
+            return el.charCodeAt( 0 );
+        } );
 
     // Should solve for smallest positive integer k
-    // let s and a be integers element of [-16,16]-{0}
+    // let s and a be integers element of [-128,128]-{0}
     // s+ka mod 256 = 0
     function cycle( s, a ) {
-        var k = 0, sum = s % 256;
-        for ( ; ( k < 256 ) && ( sum != 0 ); k++ ) {
-            sum = ( sum + a + 256 ) % 256;
+
+        // GCD variables
+        var _a = a,
+            signA = Math.sign( _a ),
+            _b = 256 * signA,
+
+            // Temp for GCD
+            t = _a,
+
+            // xGCD variables
+            x = 1,
+            u = 0,
+
+            // Temp for xGCD
+            m;
+        while ( _b !== 0 ) {
+
+            // xGCD loop
+            m = x;
+            x = u;
+            u = m - x * Math.floor( _a / _b );
+
+            // Basic GCD
+            _a = _b;
+            _b = t % _b;
+            t = _a;
         }
-        if ( sum != 0 ) {
-            return -1;
+
+        // d = _a, x = x
+
+        // k = -s * a-1 / d % ( 256 / d )
+        // if ( isInt( s / d ) ) possible;
+        if ( Number.isInteger( s / _a ) ) {
+            return ( -s * x / _a + Math.pow( 2, 16 ) ) % ( 256 / _a * signA );
         }
-        return k;
+        return -1;
     }
 
     // Should solve for smallest |i|+diff
@@ -50,65 +75,118 @@ function write( str ) {
     // let k and t be integer element of [1,255]
     // diff = |t-(k*i mod 256)|
     function targetsProps( k ) {
-        var props = [], i = -16;
-        props[0] = targets.slice();
-        props[1] = targets.slice();
-        for ( ; i <= 16; i++ ) {
+        var props = {};
+        props.diffs = targets.slice();
+        props.offts = targets.map( function() {
+            return 0;
+        } );
+        for ( var i = -16; i <= 16; i++ ) {
             var n = ( k * i + Math.pow( 2, 16 ) ) % 256;
             targets.forEach( function( el, ix ) {
-                var d = Math.abs( targets[ix] - n );
-                if ( props[0][ix] + Math.abs( props[1][ix] ) > d + Math.abs( i ) ) {
-                    props[0][ix] = d;
-                    props[1][ix] = i;
+                var d = targets[ix] - n;
+                if ( Math.abs( props.diffs[ix] ) + Math.abs( props.offts[ix] ) >
+                    Math.abs ( d ) + Math.abs( i ) ) {
+                    props.diffs[ix] = d;
+                    props.offts[ix] = i;
                 }
             } );
         }
         return props;
     }
 
-    // Listing different start, add options
+    // Listing different start & add options
+    var list = [];
     for ( var i = -128; i <= 128; i++ ) {
         for ( var j = -128; j <= 128; j++ ) {
             var k = cycle( i, j );
             if ( k > 0 ) {
-                poss.push( [ i, j, k, Math.abs( i ) + Math.abs( j ) ] );
+
+                // Add case to list
+                var obj = { start: i,
+                           add: j,
+                           cycle: k };
+                obj.char = Math.abs( i ) + Math.abs( j );
+
+                list.push( obj );
             }
         }
     }
-    poss.sort( function( a, b ) {
-        return a[3] - b[3];
-    } ).forEach( function( el ) {
-        if ( unq.indexOf( el[2] ) < 0 ) {
-            var props = targetsProps( el[2] ),
-                t = el[3] + props.reduce( function( a, b ) {
-                    var _a = a.slice().reduce( function( c, d ) {
-                            return c + d;
-                        }, 0 ),
-                        _b = b.slice().reduce( function( e, f ) {
-                            return Math.abs( e ) + Math.abs( f );
-                        }, 0 );
-                    return _a + _b;
-                } );
 
-            unq.push( el[2] );
-            buff.push( el );
-
-            el.push( props, t )
+    // Sorting by cycle, then by char
+    list = list.sort( function( a, b ) {
+        var n = a.cycle - b.cycle;
+        if ( n !== 0 ) {
+            return n;
         }
+
+        return a.char - b.char;
     } );
 
-    bfff = buff.sort( function( a, b ) {
-        return a[5] - b[5];
+    // Loop through list[] for unique [cycle]
+    var unq = [];
+    for ( var i = 0; i < list.length; ) {
+        unq.push( list[i] );
+        var n = unq.slice( -1 )[0].cycle;
+        for ( ; i < list.length; i += 1 ) {
+            if ( list[i].cycle != n ) {
+                break;
+            }
+        }
+    }
+
+    // Updating char and cycle to include diffs and offts
+    unq.forEach( function( el ) {
+            var props = targetsProps( el.cycle ),
+                d = props.diffs.reduce( function( prev, nxt ) {
+                        return prev + Math.abs( nxt );
+                    }, 0 ),
+                o = props.offts.reduce( function( prev, nxt ) {
+                        return prev + Math.abs( nxt );
+                    }, 0 );
+            el.diffs = props.diffs;
+            el.offts = props.offts;
+
+            el.char += d + o;
+            el.cycle *= o;
+        } );
+
+    // Then sort by char, then by cycle
+    unq = unq.sort( function( a, b ) {
+        var n = a.char - b.char;
+        if ( n !== 0 ) {
+            return n;
+        }
+
+        return a.cycle - b.cycle;
     } );
-    bff = bfff[0];
-    bf += add( bff[0] ) + "[";
-    for ( var i = 0; i < bff[4][1].length; i++ ) {
-        bf += ">" + add( bff[4][1][i] );
+
+    // Create Output in string form
+    var best = unq[0],
+        bf = "";
+    bf += add( best.start ) + "[";
+
+    // Declarative instead of Functional for order
+    for ( var i = 0; i < best.offts.length; i += 1 ) {
+        bf += ">" + add( best.offts[i] );
     }
-    for ( var i = 0; i < bff[4][1].length; i++ ) {
+    best.offts.forEach( function() {
         bf += "<";
+    } );
+    bf += add( best.add ) + "]>";
+
+    var curr = 0,
+        set = {};
+    for ( var i = 0; i < input.length; i += 1 ) {
+        var l = rst.indexOf( input.charAt( i ) ),
+            sign = Math.sign( l - curr );
+        for ( ; curr != l; curr += sign ) {
+            bf += sign > 0 ? ">" : "<";
+        }
+        if ( typeof set[curr] === "undefined" ) {
+            bf += add( best.diffs[curr] );
+            set[curr] = true;
+        }
+        bf += ".";
     }
-    bf += add ( bff[1] ) + "]";
-    console.log( targets, bff[4][0] );
-    return bf.replace( /1/gi, "+" );
+    return bf;
 }
